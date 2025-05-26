d7cda63238bf8348dc6c6d75ed4a10788227fab2
Chandra Kishore <chkishoreg@gmail.com>
2025-05-26 15:21:37 +0530

    update

--- image_captioner/__init__.py
+++ image_captioner/__init__.py
@@ -1,13 +1,13 @@
 from .dataset import CaptionDataset
 from .model import TransformerBridge, CLIPGPT2CaptionModel
 from .utils import save_checkpoint, load_checkpoint
-from .generate import generate_caption
+from .generate import generate_caption,show_image_with_caption
 from .feature_extractor import extract_features_from_image,extract_features_from_folder,load_clip_model
 from .preprocess import process_flickr8k
 
 __version__ = "1.0.0"
 __all__ = [
     'CaptionDataset', 'TransformerBridge', 'CLIPGPT2CaptionModel',
-    'save_checkpoint', 'load_checkpoint', 'generate_caption',
-    'extract_features', 'process_flickr8k'
+    'save_checkpoint', 'load_checkpoint', 'generate_caption','show_image_with_caption',
+    'extract_features_from_folder','extract_features_from_image', 'load_clip_model','process_flickr8k',
 ]
