6a44cf366c0b9c7d6644584f83f56edc81d8513b
Chandra Kishore <chkishoreg@gmail.com>
2025-05-30 22:27:55 +0530

    update

--- image_captioner/generate.py
+++ image_captioner/generate.py
@@ -15,7 +15,7 @@ device = config.DEVICE
 def generate_caption(
     image_path: str,
     repo_id: str = "Kishore0729/image-captioning-model",
-    filename: str = "checkpoint_epoch_1.pt",
+    filename: str = "model_3.pt",
     max_length: int = 30,
     temperature: float = 0.5,
     top_k: int = 50
